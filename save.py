1a328adb4e7ebf743f219439e9927330f1fd2c32
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-04-25 15:47:52 -0700

    Auto-commit

--- save.py
+++ save.py
@@ -8,15 +8,14 @@ def push_to_github(repo_url, token):
     subprocess.run(["git", "commit", "-m", "Auto-commit"])
 
     # Set the remote URL
-    subprocess.run(["git", "remote", "set-url", "origin", "https://github.com/" + repo_url])
+    subprocess.run(["git", "remote", "set-url", "origin", repo_url])
 
     # Authenticate with token
-    subprocess.run(["git", "config", "--global", "credential.helper", "store"])
-    subprocess.run(["git", "config", "--global", "credential.helper", f'!f() {{ echo "username=token"; echo "password={token}"; }}; f'])
+    subprocess.run(["git", "config", "--global", "credential.helper", f'!f() {{ echo "username=magentapenguin"; echo "password={token}"; }}; f'])
 
     # Push changes to the remote repository
     subprocess.run(["git", "push", "origin", "master"])
 
-repo_url = "magentapenguin/my-coding-stuff"
+repo_url = "https://github.com/magentapenguin/my-coding-stuff"
 token = "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"
 push_to_github(repo_url, token)
