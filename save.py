d13cd055dbac02694f101a0453a26a13d8385f16
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-04-25 15:33:06 -0700

    Refactor save.py to use octokit library for pushing to GitHub

--- save.py
+++ save.py
@@ -1,14 +1,15 @@
-from subprocess import run
+import octokit
 
 def push_to_github(repo_url, token):
-    run(["git", "init"], cwd="D:/coding")
-    run(["git", "add", "."], cwd="D:/coding")
-    run(["git", "commit", "-m", "'Auto-commit'"], cwd="D:/coding")
-    run(["git", "remote", "remove", "origin"], cwd="D:/coding")  # Remove the existing remote origin
-    run(["git", "remote", "add", "origin", repo_url], cwd="D:/coding")
-    run(["git", "config", "--unset", "credential.helper"], cwd="D:/coding")  # Remove the credential.helper configuration
-    run(["git", "config", "credential.helper", "store"], cwd="D:/coding")  # Set the credential.helper configuration again
-    run(["git", "push", "-u", "origin", "master"], cwd="D:/coding", env={"GIT_ASKPASS": "echo", "GITHUB_TOKEN": token})
+    repo = octokit.Repo(repo_url, token)
+    repo.init()
+    repo.add_all()
+    repo.commit("Auto-commit")
+    repo.remove_remote("origin")
+    repo.add_remote("origin", repo_url)
+    repo.unset_credential_helper()
+    repo.set_credential_helper("store")
+    repo.push("origin", "master")
 
 repo_url = "https://github.com/magentapenguin/my-coding-stuff.git"
 token = "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"
