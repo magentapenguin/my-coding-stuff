e6e83aa64fcda21bfb7150f9f60cf22a4cf84b83
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-04-25 16:17:04 -0700

    backup

--- save.py
+++ save.py
@@ -1,26 +1,23 @@
 import subprocess
 
 def push_to_github(repo_url, token):
-    # Change directory to the folder you want to backup
-    subprocess.run(["cd", "D:/coding"])
-
     # Initialize a git repository
-    subprocess.run(["git", "init"])
-    
+    subprocess.run(["git", "init"], cwd="D:/coding")
+
     # Add all changes
-    subprocess.run(["git", "add", "D:/coding"])
+    subprocess.run(["git", "add", "D:/coding"], cwd="D:/coding")
 
     # Commit changes
-    subprocess.run(["git", "commit", "-m", "backup"])
+    subprocess.run(["git", "commit", "-m", "backup"], cwd="D:/coding")
 
     # Set the remote URL
-    subprocess.run(["git", "remote", "set-url", "origin", repo_url])
+    subprocess.run(["git", "remote", "set-url", "origin", repo_url], cwd="D:/coding")
 
     # Authenticate with token
-    subprocess.run(["git", "config", "--global", "credential.helper", f'!f() {{ echo "username=magentapenguin"; echo "password={token}"; }}; f'])
+    subprocess.run(["git", "config", "--global", "credential.helper", f'!f() {{ echo "username=magentapenguin"; echo "password={token}"; }}; f'], cwd="D:/coding")
 
     # Push changes to the remote repository
-    subprocess.run(["git", "push", "origin", "main"])
+    subprocess.run(["git", "push", "origin", "main"], cwd="D:/coding")
 
 repo_url = "https://github.com/magentapenguin/my-coding-stuff"
 token = "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"
